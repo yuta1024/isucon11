041121188c5706674616a6d4457561acb998ba9b
tyabuki <yabuuuuu@gmail.com>
2021-08-21 15:36:24 +0900

    add index to isu_condition

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -28,6 +28,8 @@ CREATE TABLE `isu_condition` (
   PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
+alter table isu_condition add index nishiya(jia_isu_uuid, timestamp);
+
 CREATE TABLE `user` (
   `jia_user_id` VARCHAR(255) PRIMARY KEY,
   `created_at` DATETIME(6) DEFAULT CURRENT_TIMESTAMP(6)
